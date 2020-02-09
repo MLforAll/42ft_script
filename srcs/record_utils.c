b888b7c13bb67b59646dc167fc3605a5c9865386
Kelian DUMARAIS (MLforAll) <kdumarai@student.42.fr>
2020-02-09 07:53:20 +0100

    - almost OK -p option (works with simple_cat, kinda work with simple_bash_session) - added sample typescript files from system "script"

--- srcs/record_utils.c
+++ srcs/record_utils.c
@@ -6,7 +6,7 @@
 /*   By: kdumarai <kdumarai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/02/09 04:28:13 by kdumarai          #+#    #+#             */
-/*   Updated: 2020/02/09 04:33:22 by kdumarai         ###   ########.fr       */
+/*   Updated: 2020/02/09 07:48:56 by kdumarai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,13 +19,13 @@ t_rts_record	*next_record(t_rts *rts, t_rts_record *r)
 	t_rts_record	*ret;
 
 	s = (char *)&r->start;
-	while (*s != 0x1 && *s != 0x2)
+	while (*s && *s != 0x1 && *s != 0x2 && *s != 0x3)
 	{
 		if ((void *)s > rts->endptr)
 			return (NULL);
 		s++;
 	}
-	ret = (t_rts_record *)(s + sizeof(r->start));
+	ret = (t_rts_record *)(s + (*s ? 4 : 3));
 	return ((void *)ret > rts->endptr ? NULL : ret);
 }
 
@@ -38,7 +38,7 @@ t_uint8			get_record_string(t_rts *rts, \
 
 	s = (char *)&r->start;
 	len = 0;
-	while (s[len] != 0x1 && s[len] != 0x2)
+	while (s[len] && s[len] != 0x1 && s[len] != 0x2 && s[len] != 0x3)
 	{
 		if ((void *)(s + len) > rts->endptr)
 			return (FALSE);
